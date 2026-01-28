d112cc1dc750251e3527c42da9b8e3756a4e98f0
Corey Hickson <chickson@hmc.edu>
2026-01-27 17:40:10 -0800

    Remove depricated operation encodings

--- coverage/covergroups/bins_templates/conversion_op_bins.svh
+++ coverage/covergroups/bins_templates/conversion_op_bins.svh
@@ -1,7 +1,3 @@
         // all conversion operations
         bins op_cfi    = {[OP_CFI  : OP_CFI  | 32'hF]};
-        bins op_fcvtw  = {OP_FCVTW};
-        bins op_fcvtwu = {OP_FCVTWU};
-        bins op_fcvtl  = {OP_FCVTL};
-        bins op_fcvtlu = {OP_FCVTLU};
         bins op_cff    = {[OP_CFF   : OP_CFF   | 32'hF]}; 
